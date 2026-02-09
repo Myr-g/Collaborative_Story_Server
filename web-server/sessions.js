a709c1ded4f6d2a8d3fb6df54c5273c61e32e2e9
cbgalileo <cbrice@plu.edu>
2026-02-09 14:47:52 -0800

    implemented sessions.js
    createSession:
    - checks if name already exists and returns false if so,
    - otherwise, will create a new session object, add it to the maps, then return the new session
    
    getSessionById & getSessionByName:
    - getters
    
    addUserToSession:
    - checks if the passed session id exists in a map, if not returns null
    - if it does, create a new user, places them into the session, and returns the session and user
    
    removeUserFromSession:
    - checks if the passed session id exists in a map, if not returns false
    - also return false if trying to delete the user fails
    - after deletion, if the session has no users, delete it from both maps and return true

---
+++ web-server/sessions.js
@@ -0,0 +1,105 @@
+const { get } = require("https");
+const { generateSessionId, generateUserId } = require("./utils/ids");
+
+const sessions_by_id = new Map(); // key is the session id, value is the session
+const sessions_by_name = new Map(); //key is the session name, value is the session id
+
+function createSession(name, genre)
+{
+    const display_name = name;
+    const normalized_name = name.toLowerCase();
+
+    if(sessions_by_name.has(normalized_name))
+    {
+        return null;
+    }
+
+    const session_id = generateSessionId();
+
+    let new_session = {
+        id: session_id,
+        name: display_name,
+        genre: genre,
+        story: "",
+        users: new Map(),
+        createdAt:new Date().toISOString()
+    };
+
+    sessions_by_id.set(session_id, new_session);
+    sessions_by_name.set(normalized_name, session_id);
+
+    return new_session;
+}
+
+function getSessionById(session_id)
+{
+    if(sessions_by_id.has(session_id))
+    {
+        return sessions_by_id.get(session_id);
+    }
+
+    else
+    {
+        return null;
+    }
+}
+
+function getSessionByName(session_name)
+{
+    const normalized_name = session_name.toLowerCase();
+
+    if(sessions_by_name.has(normalized_name))
+    {
+        return getSessionById(sessions_by_name.get(normalized_name));
+    }
+
+    else
+    {
+        return null;
+    }
+}
+
+function addUserToSession(session_id, username)
+{
+    const session = getSessionById(session_id);
+
+    if(!session)
+    {
+        return null;
+    }
+
+    const user_id = generateUserId();
+
+    let new_user = {
+        id: user_id,
+        name: username,
+        joinedAt: new Date().toISOString()
+    };
+
+    session.users.set(user_id, new_user);
+
+    return {session, user: new_user};
+}
+
+function removeUserFromSession(session_id, user_id)
+{
+    const session = getSessionById(session_id);
+
+    if(!session)
+    {
+        return false;
+    }
+
+    if(!session.users.delete(user_id))
+    {
+        return false;
+    }
+
+    if(session.users.size === 0)
+    {
+        sessions_by_id.delete(session_id);
+        sessions_by_name.delete(session.name.toLowerCase());
+    }
+
+    return true;
+}
