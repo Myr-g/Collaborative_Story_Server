1c0157f58c6342acb8d07a8f8c604b39341a65a3
cbgalileo <cbrice@plu.edu>
2026-03-16 14:32:45 -0700

    half-implemented story_manager.js
    story_manager.js will handle individual story functions such as load stories from local storage, saving them, and deleting them to reduce the amount of repetitive calls from home.js and session.js

---
+++ web-app/public/story_manager.js
@@ -0,0 +1,91 @@
+const crypto = require("crypto");
+
+const storage_key = "stories";
+
+function loadStories()
+{
+    const storage = localStorage.getItem(storage_key);
+
+    if(!storage)
+    {
+        return [];
+    }
+
+    return JSON.parse(storage);
+}
+
+function saveStories(stories)
+{
+    localStorage.setItem(storage_key, JSON.stringify(stories));
+}
+
+function getStories()
+{
+    return loadStories();
+}
+
+function getStory(id)
+{
+    const stories = loadStories();
+
+    for(const story of stories)
+    {
+        if(story.id === id)
+        {
+            return story;
+        }
+    }
+
+    return null;
+}
+
+function createStory(data)
+{
+    const stories = loadStories();
+
+    const new_story = {
+        id: "stry_" + crypto.randomUUID(),
+        title: data.title,
+        genre: data.genre,
+        promptType: data.promptType,
+        prompt: data.prompt,
+        content: "",
+        createdAt: new Date().toISOString(),
+        updatedAt: null
+    };
+
+    stories.push(new_story);
+    saveStories(stories);
+
+    return new_story;
+}
+
+function saveStory(story)
+{
+    const stories = loadStories();
+
+    for(let i = 0; i < stories.length; i++)
+    {
+        if(stories[i].id === story.id)
+        {
+            story.updatedAt = new Date().toISOString();
+            stories[i] = story;
+            break;
+        }
+    }
+
+    saveStories();
+}
+
+function deleteStory(id)
+{
+
+}
+
+window.StoryManager = {
+    getStories,
+    getStory,
+    createStory,
+    saveStory,
+    deleteStory
+};
